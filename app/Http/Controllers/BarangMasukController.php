86678e9a513dd50cdcd7ea518066fdebe5081a47
RulH19 <nurulh298hd@gmail.com>
2023-12-29 16:22:51 +0700

    success crud barang masuk

--- app/Http/Controllers/BarangMasukController.php
+++ app/Http/Controllers/BarangMasukController.php
@@ -9,9 +9,9 @@ class BarangMasukController extends Controller
 {
     public function index()
     {
-        $barangMasuk = BarangMasuk::get();
+        $barang = BarangMasuk::get();
 
-        return view('barangMasuk.index', ['data' => $barangMasuk]);
+        return view('barangMasuk.index', ['data' => $barang]);
     }
     public function tambah()
     {
@@ -29,6 +29,31 @@ class BarangMasukController extends Controller
         ];
 
         BarangMasuk::create($data);
+
+        return redirect()->route('barangMasuk');
+    }
+    public function edit($id)
+    {
+        $barang = BarangMasuk::find($id)->first();
+        return view('barangMasuk.form', ['barang' => $barang]);
+    }
+    public function update($id, Request $request)
+    {
+        $data = [
+            'id_barang' => $request->id_barang,
+            'nama_barang' => $request->nama_barang,
+            'harga' => $request->harga,
+            'stok' => $request->stok,
+            'nama_penerima' => $request->nama_penerima
+        ];
+
+        BarangMasuk::find($id)->update($data);
+
+        return redirect()->route('barangMasuk');
+    }
+    public function hapus($id)
+    {
+        BarangMasuk::find($id)->delete();
 
         return redirect()->route('barangMasuk');
     }
--- resources/views/barangMasuk/form.blade.php
+++ resources/views/barangMasuk/form.blade.php
@@ -1,34 +1,34 @@
 @extends('layout.app')
 @section('title','Form Barang')
 @section('content')
-<form action="{{ isset($barangMasuk) ?route('barangMasuk.tambah.update', $barangMasuk->id) : route('barangMasuk.tambah.simpan') }}" method="post">
+<form action="{{ isset($barang) ?route('barangMasuk.tambah.update', $barang->id) : route('barangMasuk.tambah.simpan') }}" method="post">
     @csrf
     <div class="row">
         <div class="col-12">
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary"> {{  isset($barangMasuk) ? 'Form Edit Barang Masuk':'Form Tambah Barang Masuk' }}</h6>
+                    <h6 class="m-0 font-weight-bold text-primary"> {{  isset($barang) ? 'Form Edit Barang Masuk':'Form Tambah Barang Masuk' }}</h6>
                 </div>
                 <div class="card-body">
                     <div class="form-group">
                         <label for="id_barang">Id Barang</label>
-                        <input type="text" class="form-control" id="id_barang" name="id_barang" value="{{ isset($barangMasuk) ? $barangMasuk->id_barang :'' }}">
+                        <input type="text" class="form-control" id="id_barang" name="id_barang" value="{{ isset($barang) ? $barang->id_barang :'' }}">
                     </div>
                     <div class="form-group">
                         <label for="nama_barang">Nama Barang</label>
-                        <input type="text" class="form-control" id="nama_barang" name="nama_barang" value="{{ isset($barangMasuk) ? $barangMasuk->nama_barang :'' }}">
+                        <input type="text" class="form-control" id="nama_barang" name="nama_barang" value="{{ isset($barang) ? $barang->nama_barang :'' }}">
                     </div>
                     <div class="form-group">
                         <label for="harga">Harga Barang</label>
-                        <input type="text" class="form-control" id="harga" name="harga" value="{{ isset($barangMasuk) ? $barangMasuk->harga :'' }}">
+                        <input type="text" class="form-control" id="harga" name="harga" value="{{ isset($barang) ? $barang->harga :'' }}">
                     </div>
                     <div class="form-group">
                         <label for="stok">stok Barang</label>
-                        <input type="number" class="form-control" id="stok" name="stok" value="{{ isset($barangMasuk) ? $barangMasuk->stok :'' }}">
+                        <input type="number" class="form-control" id="stok" name="stok" value="{{ isset($barang) ? $barang->stok :'' }}">
                     </div>
                     <div class="form-group">
                         <label for="nama_penerima">Nama Penerima</label>
-                        <input type="text" class="form-control" id="nama_penerima" name="nama_penerima" value="{{ isset($barangMasuk) ? $barangMasuk->nama_penerima :'' }}">
+                        <input type="text" class="form-control" id="nama_penerima" name="nama_penerima" value="{{ isset($barang) ? $barang->nama_penerima :'' }}">
                     </div>
                 </div>
                 <div class="card-footer">
